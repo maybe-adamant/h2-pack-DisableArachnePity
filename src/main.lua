77bd525c5f75d7cb69e2c25a7cb6a73f4d61544a
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:02 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Disables Arachne Pity entirely for Anyfear runs.",
     default  = false,
     dataMutation = true,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
